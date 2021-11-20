31ff097e6ee2a2017ea9d1ac28bec23302f3adfc
munroe1786 <era3munroe@gmail.com>
2021-11-19 19:01:48 -0600

    import TodoContainer into index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoContainer from './Components/TodoContainer';
 
-const element = <h1>Hello from Create React App!</h1>
+//const element = <h1>Hello from Create React App!</h1>
 
 ReactDOM.render(
-  element,
+  <TodoContainer />,
   document.getElementById('root')
 );
