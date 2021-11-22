0eb1aa36426c54c290c679cca0017cdbb72e0493
munroe1786 <era3munroe@gmail.com>
2021-11-21 21:12:19 -0600

    change div in TodoContainer to fragment and add StrictMode to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import TodoContainer from './Components/TodoContainer';
 //const element = <h1>Hello from Create React App!</h1>
 
 ReactDOM.render(
-  <TodoContainer />,
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
   document.getElementById('root')
 );
