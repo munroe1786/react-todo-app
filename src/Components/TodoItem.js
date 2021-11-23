b0035d8de5b17a549d14288c60570da0df3212d4
munroe1786 <era3munroe@gmail.com>
2021-11-22 21:49:24 -0600

    build out TodoItem.js

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <div>Hello!</div>
+        )
+    }
+}
+
+export default TodoItem;
